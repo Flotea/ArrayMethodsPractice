55182086f939c9869d4971b4179209802722dbff
flotea <hgrimmer0786@myhta.org>
2019-04-24 11:12:46 -1000

    finished

---
+++ app.js
@@ -0,0 +1,19 @@
+var grateful = ["my family", "my friends", "my education"]
+grateful.pop();
+console.log(grateful);
+grateful.push("my house", "my job");
+console.log(grateful);
+grateful.shift();
+console.log(grateful);
+grateful.unshift("my parents");
+console.log(grateful);
+
+var pride = ["my grades", "being able to talk to more people"];
+pride.pop();
+console.log(pride);
+pride.push("my projects", "signing up for english 100");
+console.log(pride);
+pride.shift();
+console.log(pride);
+pride.unshift("trying new things");
+console.log(pride);
